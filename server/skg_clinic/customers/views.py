0b5f43b55f58317f60bd8e048e8cde23ede830b5
Nitesh Rauniyar <niteshrauniyar0@gmail.com>
2026-01-01 17:45:23 +0530

    customers module has done

--- server/skg_clinic/customers/views.py
+++ server/skg_clinic/customers/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import PetProfile
+from .serializers import PetProfileSerializer
+from users.permissions import IsStaff
 
-# Create your views here.
+
+class PetProfileViewSet(ModelViewSet):
+    serializer_class = PetProfileSerializer
+    permission_classes = [IsStaff]
+
+    def get_queryset(self):
+        return PetProfile.objects.filter(is_active=True)
+
+    def perform_destroy(self, instance):
+        # Soft delete
+        instance.is_active = False
+        instance.save()
